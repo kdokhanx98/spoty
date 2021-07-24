6a16b0659776bc007631b4c6b2aacbe27d23df27
Khaled Dokhan <k.dokhan@outlook.com>
2021-07-24 14:50:45 +0300

    subscription screen design

--- lib/screens/subscription_screen.dart
+++ lib/screens/subscription_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -6,159 +9,216 @@ class SubscriptionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: HexColor("#EF4A51"),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 32.0),
-          child: Column(
-
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(25)),
-                        color: Colors.white,
-                      ),
-
-                      child: IconButton(
-                        iconSize: 20,
-                        color: Colors.redAccent,
-                        onPressed: () {
-                        Navigator.pop(context);
-                      }, icon: const Icon(Icons.close),),
-                    ),
-                  ],
-                ),
-              ),
-
-              const Center(
-                child: Text("Unlock Everything",
-                  style:
-                  TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
-                child: Text(
-                  "Track your child, see his latest location and much more!",
-                  textAlign: TextAlign.center, style:
-                TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),),
-              ),
-
-              Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(25)),
-                ),
-                width: 150,
-                height: 180,
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 90,
-                    decoration:  BoxDecoration(
-                      color: HexColor("#EBEBEB"),
-                      borderRadius: const BorderRadius.only(topRight: Radius.circular(25), topLeft: Radius.circular(25)),
-                    ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(child:
-                        Text("1\nYEAR", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.grey),)
-                        ),
-                      ),
-                    ),
-                    const Spacer(),
-                     Container(
-                      height: 90,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(25), bottomLeft: Radius.circular(25)),
-                      ),
-                      child:  Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          // ignore: prefer_const_literals_to_create_immutables
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover),
+            ),
+          ),
+          ClipRRect(
+            // Clip it cleanly.
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 32.0),
+                  child: Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 24.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                             const Text("\$99.99", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-                             const Padding(
-                               padding: EdgeInsets.only(top: 4.0),
-                               child:  Text("per year", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey),),
-                             ),
-
+                            const Text(
+                              "Restore",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25)),
+                                color: Colors.transparent,
+                              ),
+                              child: IconButton(
+                                iconSize: 30,
+                                color: Colors.white,
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: const Icon(Icons.close),
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                    ),
-
-                  ],
+                      const Padding(
+                        padding: EdgeInsets.only(top: 60.0),
+                        child: Text(
+                          "Unlimited Access\nto All Features",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 36),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                        child: Center(
+                          child: ClipRect(
+                            child: BackdropFilter(
+                              filter:
+                                  ImageFilter.blur(sigmaX: 6.0, sigmaY: 6.0),
+                              child: Container(
+                                width: size.width * 0.9,
+                                height: size.height * 0.08,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(50)),
+                                  color: Colors.grey.shade700.withOpacity(0.2),
+                                  border: Border.all(
+                                      width: 1, color: Colors.black54),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16.0, vertical: 8),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    // ignore: prefer_const_literals_to_create_immutables
+                                    children: [
+                                      Text(
+                                        '3-DAY FREE TRIAL',
+                                        style: TextStyle(
+                                          color: HexColor("#A6A9AE"),
+                                        ),
+                                      ),
+                                      RichText(
+                                        text: const TextSpan(
+                                          style: TextStyle(
+                                            fontSize: 16.0,
+                                            color: Colors.white,
+                                          ),
+                                          children: [
+                                            TextSpan(
+                                              text: '\$99.99',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            TextSpan(
+                                                text:
+                                                    ' per year, auto-renewable'),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: size.height * 0.08,
+                      ),
+                      const Text(
+                        "Upgrade & Unlock\nEverything with features",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      const Text(
+                        "No Commitment, Cancel Anytime.",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: size.height * 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: Container(
+                          width: size.width * 0.9,
+                          height: size.height * 0.1,
+                          decoration: BoxDecoration(
+                            color: HexColor("#E85F67"),
+                            border: Border.all(
+                                width: 1, color: HexColor("#E85F67")),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(50)),
+                          ),
+                          child:  Center(
+                            child: GestureDetector(
+                              onTap: (){},
+                              child: const ListTile(
+                                title: Text(
+                                  "Continue",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400),
+                                  textAlign: TextAlign.center,
+                                ),
+                                trailing: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: size.height * 0.06,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 64.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            GestureDetector(
+                              onTap: (){},
+                                child: const Text(
+                              "Terms of Services",
+                              style: TextStyle(
+                                color: Colors.white,
+                                decoration: TextDecoration.underline,
+                              ),
+                            )),
+                            GestureDetector(
+                              onTap: (){},
+                              child: const Text(
+                                "Privacy Policy",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 64),
-                child: Text(
-                  "Includes 3-days free trial, Cancel anytime.",
-                  textAlign: TextAlign.center, style:
-                TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),),
-              ),
-
-               Padding(
-                 padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
-                 child: GestureDetector(
-                   onTap: (){
-                     print("continue clicked");
-                   },
-                   child: Container(
-                     height: 60,
-                     width: double.infinity,
-                     decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(25)),
-                       color: Colors.white,
-                     ),
-                    child: const Center(
-                      child:  Text(
-                        "Continue",
-                        style: TextStyle(color: Colors.red, fontSize: 24, fontWeight: FontWeight.w500),
-                      ),
-                    ),
-              ),
-                 ),
-               ),
-
-              const Spacer(),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 32),
-                child: Text(
-                  "By continuing, you agree to our\nTerms of Service and Privacy Policy",
-                  textAlign: TextAlign.center, style:
-                TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                ),),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
